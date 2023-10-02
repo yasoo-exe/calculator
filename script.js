5ab702688f55d4ff696adbb3d7b9fc40f5c82221
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2023-10-02 13:45:36 +0500

    change the mathematical functions in a way so they can be called through method chaining

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-let sum = (a, b) => {
-  return Math.round((a + b) * 1000) / 1000;
-};
+function operations() {
+  return {
+    add: (a, b) => Math.round((a + b) * 1000) / 1000,
+    subtract: (a, b) => Math.round((a - b) * 1000) / 1000,
+    multiply: (a, b) => Math.round(a * b * 1000) / 1000,
+    divide: (a, b) => Math.round((a / b) * 1000) / 1000,
+  };
+}
 
-let subtract = (a, b) => {
-  return Math.round((a - b) * 1000) / 1000;
-};
-
-let multiply = (a, b) => {
-  return Math.round(a * b * 1000) / 1000;
-};
-
-let divide = (a, b) => {
-  return Math.round((a / b) * 1000) / 1000;
-};
+let calculate = operations();
 
 let inputBox = document.querySelector(".input-container input");
 
+//dynamic expression updates
 let digits = document.querySelectorAll(".digits");
 Array.from(digits).forEach((x) =>
   x.addEventListener("click", () => {
@@ -23,10 +19,12 @@ Array.from(digits).forEach((x) =>
   })
 );
 
+//clear
 document.querySelector(".clear").addEventListener("click", () => {
   inputBox.value = "";
 });
 
+//backspace
 document.querySelector(".backspace").addEventListener("click", () => {
   let tobeCleared = inputBox.value;
   inputBox.value = tobeCleared
