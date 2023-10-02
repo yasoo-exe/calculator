74a432801f3c7c33e0ec7dafcae983e3a683d9c6
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2023-10-02 13:29:21 +0500

    added functionality for clear, backspace and input box dynamic update

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-let sum = () => {
-  return a + b;
+let sum = (a, b) => {
+  return Math.round((a + b) * 1000) / 1000;
 };
 
-let subtract = () => {
-  return a - b;
+let subtract = (a, b) => {
+  return Math.round((a - b) * 1000) / 1000;
 };
 
-let multiply = () => {
-  return a * b;
+let multiply = (a, b) => {
+  return Math.round(a * b * 1000) / 1000;
 };
 
-let divide = () => {
-  return a / b;
+let divide = (a, b) => {
+  return Math.round((a / b) * 1000) / 1000;
 };
+
+let inputBox = document.querySelector(".input-container input");
+
+let digits = document.querySelectorAll(".digits");
+Array.from(digits).forEach((x) =>
+  x.addEventListener("click", () => {
+    inputBox.value += x.innerText;
+  })
+);
+
+document.querySelector(".clear").addEventListener("click", () => {
+  inputBox.value = "";
+});
+
+document.querySelector(".backspace").addEventListener("click", () => {
+  let tobeCleared = inputBox.value;
+  inputBox.value = tobeCleared
+    .split("")
+    .splice(0, tobeCleared.length - 1)
+    .join("");
+});
