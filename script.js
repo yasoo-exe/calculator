dd11148641d81eabf15ed3fed5d309e20ef44509
yasoo-exe <76653084+yasoo-exe@users.noreply.github.com>
2023-10-03 12:14:09 +0500

    changed layout, made functions work
    operations on calculator working except the equal button, answer and expression input boxes separated

--- script.js
+++ script.js
@@ -1,15 +1,104 @@
-function operations() {
+"use strict";
+
+function operations(operand, first, second) {
   return {
-    add: (a, b) => Math.round((a + b) * 1000) / 1000,
-    subtract: (a, b) => Math.round((a - b) * 1000) / 1000,
-    multiply: (a, b) => Math.round(a * b * 1000) / 1000,
-    divide: (a, b) => Math.round((a / b) * 1000) / 1000,
+    "+": (a, b) => Math.round((a + b) * 1000) / 1000,
+    "-": (a, b) => Math.round((a - b) * 1000) / 1000,
+    "*": (a, b) => Math.round(a * b * 1000) / 1000,
+    "/": (a, b) =>
+      b === 0 ? "ain't gonna happen!" : Math.round((a / b) * 1000) / 1000,
   };
 }
 
-let calculate = operations();
+let operate = operations();
+let inputBox = document.querySelector("#expression");
+let answer = document.querySelector("#answer");
+let operators = Array.from(document.querySelectorAll(".operators"));
+let equal = document.querySelector(".equal");
 
-let inputBox = document.querySelector(".input-container input");
+let a, b, operand, result;
+let count = 0;
+
+operators.forEach((x) => x.addEventListener("click", evaluateExpression));
+function evaluateExpression() {
+  if (count === 0 && inputBox.value !== "") {
+    a = Number(inputBox.value);
+    operand = this.innerText;
+    inputBox.value = "";
+    count += 1;
+  }
+  if (count === 1 && inputBox.value !== "") {
+    b = Number(inputBox.value);
+    inputBox.value = "";
+    a = operate[`${operand}`](a, b);
+    answer.value = a;
+    operand = this.innerText;
+  }
+}
+
+// equal.addEventListener("click", evaluateExpression);
+
+// function evaluateExpression() {
+//   if (/^[\d]+(-|\+|\*|\/){1}[\d]+/.test(inputBox.value)) {
+//     console.log("valid!");
+//   }
+// }
+
+// let a, b, operand;
+// let count = 0;
+// function evaluateExpression() {
+//   if (inputBox.value !== "" && /[\d]/g.test(inputBox.value) && count === 0) {
+//     a = Number(inputBox.value);
+//     operand = this.innerText;
+//     inputBox.value = "";
+//     count += 1;
+//   }
+//   if (a && count === 1 && /[\d]/.test(inputBox.value)) {
+//     b = Number(inputBox.value);
+//     count += 1;
+//     if (a & b) {
+//       let result = calculate[`${operand}`](a, b);
+//       inputBox.value = result;
+//       count === 0;
+//     }
+//   }
+//   if (a && b && count === 3) {
+//     console.log(calculate.operand(a, b));
+//   }
+// }
+
+// let validInputValues = [
+//   "1",
+//   "2",
+//   "3",
+//   "4",
+//   "5",
+//   "6",
+//   "7",
+//   "8",
+//   "9",
+//   "0",
+//   "-",
+//   "+",
+//   "*",
+//   "/",
+//   "Backspace",
+//   "ArrowRight",
+//   "ArrowLeft",
+//   "Enter",
+//   "CtrlA",
+// ];
+// let validButtonActions = ["Backspace", "ArrowRight", "ArrowLeft", "Enter"];
+
+//avoid falsy values to be entered via keyboard
+// inputBox.addEventListener("keydown", (e) => {
+//   if (validInputValues.indexOf(e.key) === -1) {
+//     e.preventDefault();
+//   }
+//   if (e.key === "Enter") {
+//     evaluateExpression();
+//   }
+// });
 
 //dynamic expression updates
 let digits = document.querySelectorAll(".digits");
@@ -22,6 +111,8 @@ Array.from(digits).forEach((x) =>
 //clear
 document.querySelector(".clear").addEventListener("click", () => {
   inputBox.value = "";
+  answer.value = "";
+  count = 0;
 });
 
 //backspace
